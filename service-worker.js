4f21d2478a4ee4f8c52aa73d937a5c50926d4645
clearsky778 <banana-sin@hanmail.net>
2026-07-16 22:41:52 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'etf-rebalancing-v19';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon.svg',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+
+  if(url.origin!==self.location.origin){
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  if(event.request.mode==='navigate'){
+    event.respondWith(
+      fetch(event.request).then(response=>{
+        if(response&&response.ok){
+          const copy=response.clone();
+          return caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy)).catch(()=>{}).then(()=>response);
+        }
+        return response;
+      }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html')))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+      if(!response||!response.ok)return response;
+      const copy=response.clone();
+      return caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy)).catch(()=>{}).then(()=>response);
+    }).catch(()=>cached))
+  );
+});
